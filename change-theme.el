95b1a7a3d737d5436dbd29068ecf25c135920a0d
Jon Harder <jharder@telsasoft.com>
2015-10-22 15:07:21 -0500

    renamed package name

--- change-theme.el
+++ change-theme.el
@@ -1,4 +1,4 @@
-;;; theme-changer.el --- wrapper around changing themes for custom settings   -*- lexical-binding: t -*-
+;;; change-theme.el --- wrapper around load-theme for custom settings   -*- lexical-binding: t -*-
 
 ;; Author: Jon Harder <jonharder6@gmail.com>
 ;; Version: 1.0
@@ -21,44 +21,44 @@
     nil))
 
 
-(defvar theme-changer-themes (make-hash-table)
+(defvar change-theme-themes (make-hash-table)
   "the hash table containing all the themes the user will create.
 Themes are added in the form (puthash theme-name (lambda () ,@init-code))")
 
 
-(defun theme-changer-available-themes ()
+(defun change-theme-available-themes ()
   (interactive)
   (let ((theme-names '()))
-    (maphash (lambda (key _) (push (symbol-name key) theme-names)) theme-changer-themes)
+    (maphash (lambda (key _) (push (symbol-name key) theme-names)) change-theme-themes)
     theme-names))
 
 
-(defmacro theme-changer-make-theme (name &rest init-code)
+(defmacro change-theme-make-theme (name &rest init-code)
   "creates a theme (really just a symbol for the name which is used by `load-theme' in
-`theme-changer-set-theme', and a lambda for the initialization code when that theme
-is selected.  It also adds that theme to `theme-changer-themes'."
+`change-theme-set-theme', and a lambda for the initialization code when that theme
+is selected.  It also adds that theme to `change-theme-themes'."
   `(if (theme-available-p ,name)
-       (progn (puthash ,name (lambda () ,@init-code) theme-changer-themes)
+       (progn (puthash ,name (lambda () ,@init-code) change-theme-themes)
 	      (message (concat "Added theme " (symbol-name ,name))))
      (message (concat "Theme '" (symbol-name ,name) "' not available for load-theme"))))
 
 
-(defun theme-changer-set-theme (theme)
+(defun change-theme-set-theme (theme)
   "sets the theme to the THEME. If called interactively it will use
-ido to select from the list of the themes added so far via `theme-changer-make-theme'."
+ido to select from the list of the themes added so far via `change-theme-make-theme'."
   (interactive
    (list (ido-completing-read
 	  "Select theme: "
-	  (theme-changer-available-themes))))
+	  (change-theme-available-themes))))
   (when (stringp theme)
     (setq theme (intern theme)))
-  (if (memq (symbol-name theme) (theme-changer-available-themes))
+  (if (memq (symbol-name theme) (change-theme-available-themes))
       (progn (dolist (tt custom-enabled-themes)
 	       (disable-theme tt))
 	     (load-theme theme)
-	     (funcall (gethash theme theme-changer-themes)))
+	     (funcall (gethash theme change-theme-themes)))
     (message (concat "Theme '" (symbol-name theme) "' not a recognized theme"))))
 
 
-(provide 'theme-changer)
-;;; theme-changer ends here
+(provide 'change-theme)
+;;; change-theme ends here
